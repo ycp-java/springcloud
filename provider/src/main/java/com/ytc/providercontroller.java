07e0c48b15587b848c42d489a9d8f4b300a4ec53
ycp-java <2315316031@qq.com>
2020-10-03 15:52:50 +0800

    第er次提交

---
+++ provider/src/main/java/com/ytc/providercontroller.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: providercontroller
+ * Author:   ycp
+ * Date:     2020/10/3 15:52
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ytc;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author ycp
+ * @create 2020/10/3
+ * @since 1.0.0
+ */
+public class providercontroller {
+
+}
